e3364671aaffffc92af8da147625df3f4a6fbc08
NitishKumarNanda <71919600+NitishKumarNanda@users.noreply.github.com>
2023-03-21 06:00:18 +0530

    rwerwerwer

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import "../styles/App.css";
 import Child from "./Child";
 const App = () => {
-    const [age, setAge] = useState(18);
+    const [age, setAge] = useState(19);
     function incrementAge() {
       setAge(age + 1);
     }
